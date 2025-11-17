35be74e6432b018e40a0ae3ab895e4679d5e0dd9
Omogo Peter <omogopeter48@gmail.com>
2025-11-17 07:31:47 +0100

    Add new files

--- todo-app/resources/views/layouts/app.blade.php
+++ todo-app/resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width,initial-scale=1" />
   <meta name="csrf-token" content="{{ csrf_token() }}">
-  <title>To-Do — Modern AJAX</title>
+  <title>To-Do — Website</title>
   <style>
     * {
       margin: 0;
